c478febed13a81c9c552eab044a33f78f983a723
KapilLanjewar <37495732+KapilLanjewar@users.noreply.github.com>
2020-07-30 07:57:54 +0530

    Add files via upload

--- spring-r2dbc-courses/src/main/resources/data.sql
+++ spring-r2dbc-courses/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO COURSE (ID,TITLE,AUTHOR,CATEGORY) VALUES (1,'Java Complete Reference','Herbert Schildt', 'Java');
-INSERT INTO COURSE (ID,TITLE,AUTHOR,CATEGORY) VALUES (2,'Building a JavaScript Devlopment Environment','Cory House', 'JavaScript');
-INSERT INTO COURSE (ID,TITLE,AUTHOR,CATEGORY) VALUES (3,'Microservices Architecture','Martic Fowler', 'Microservices');
+INSERT INTO COURSE (TITLE,AUTHOR,CATEGORY) VALUES ('Java Complete Reference','Herbert Schildt', 'Java');
+INSERT INTO COURSE (TITLE,AUTHOR,CATEGORY) VALUES ('Building a JavaScript Devlopment Environment','Cory House', 'JavaScript');
+INSERT INTO COURSE (TITLE,AUTHOR,CATEGORY) VALUES ('Microservices Architecture','Martic Fowler', 'Microservices');
